ef7679c788981d237fd18d35da3ddac6b9fdb839
Gus (Nala) <gusrosales7@gmail.com>
2020-05-29 23:23:12 -0700

    new footer

--- client/src/components/Foot/index.js
+++ client/src/components/Foot/index.js
@@ -3,9 +3,42 @@ import "./style.css";
 
 function Foot() {
   return (
-    <div className="footer">
-      <h3>Copyright, right?</h3>
-    </div>
+    <footer className="footer">
+      <div className="footer-left">2020 &copy; gr\^_^/</div>
+      <div className="footer-icons">
+        <ul className="menu simple">
+          <li>
+            <a href="https://github.com/gusvalenzuela" target="_blank"  rel="noopener noreferrer">
+              <i className="fa fa-github" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/gus-valenzuela-b73b0296/"
+              target="_blank"  rel="noopener noreferrer"
+            >
+              <i className="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#target-1" target="_blank"  rel="noopener noreferrer">
+              <i className="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#target-1" target="_blank"  rel="noopener noreferrer">
+              <i className="fa fa-pinterest-p" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#target-1" target="_blank"  rel="noopener noreferrer">
+              <i className="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li></li>
+        </ul>
+      </div>
+    </footer>
   );
 }
 
