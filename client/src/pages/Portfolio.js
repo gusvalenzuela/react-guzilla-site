674316514ddf902c0611becb52480bd496cf73fe
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-06-11 16:44:14 -0700

    removing last updated_at for now

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment"
+import github from "octonode"
 import API from "../utils/API";
 import "./mainstyle.css";
 import { Row } from "../components/Grid";
@@ -7,6 +8,7 @@ import Card from "../components/Card";
 import localProjects from "../utils/localProjects.json";
 import Foot from "../components/Foot";
 import ResumeCard from "../components/ResumeCard";
+const ghclient = github.client()
 
 function Portfolio({ handleOpeningSidebar }) {
   // Setting our component's initial state
@@ -22,18 +24,24 @@ function Portfolio({ handleOpeningSidebar }) {
   useEffect(() => {
     loadProjects();
 
-    API.getGitUpdateData().then((gitData) => {
-      gitData.data.forEach((item) => {
-        projects.forEach((proj) => {
-          if (proj.repo_name === item.name) {
-            proj.updated_at = item.updated_at
-            console.log(`${item.name}\r\n last updated ${item.updated_at}`);
-          }
-        });
+    let repo = ghclient.repo(`gusvalenzuela/quiz-game`)
 
-        setProjects(projects)
-      });
-    });
+    repo.info((data) => {
+      console.log(data)
+    })
+
+    // API.getGitUpdateData().then((gitData) => {
+    //   gitData.data.forEach((item) => {
+    //     projects.forEach((proj) => {
+    //       if (proj.repo_name === item.name) {
+    //         proj.updated_at = item.updated_at
+    //         console.log(`${item.name}\r\n last updated ${item.updated_at}`);
+    //       }
+    //     });
+
+    //     setProjects(projects)
+    //   });
+    // });
 
   }, []);
 
@@ -47,7 +55,7 @@ function Portfolio({ handleOpeningSidebar }) {
               <h3>{Project.title}</h3>
               <p style={{ fontSize: "14px" }}>{Project.libraries}</p>
               <p>{Project.lead}</p>
-              <p>Last updated {`${moment(Project.updated_at).calendar()}`}</p>
+              {/* <p>Last updated {`${moment(Project.updated_at).calendar()}`}</p> */}
               <a href={Project.app_url}>
                 <i className="fa fa-chevron-right"> app</i>
               </a>
