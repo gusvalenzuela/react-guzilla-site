910d4709174e26dc251fed04ab88e0792d9045c2
guzilla <gusrosales7@gmail.com>
2020-09-01 20:29:54 -0700

    Update mongo connection to Atlas

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URX, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
